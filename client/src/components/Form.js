8eed3c89ead9a32d86382142ef6c9b31b1bcb9f9
asketsystem <asketsystem1@gmail.com>
2022-03-05 14:34:16 +0000

    Building the component layout

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
+import { AlertError } from "./views/AlertError";
+import { AlertSuccess } from "./views/AlertSuccess";
 
 export const Form = () => {
   const initialFormData = {
@@ -57,5 +59,46 @@ export const Form = () => {
     setFormSuccess("");
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit} className="form">
+          <h1>React form</h1>
+          <AlertSuccess success={formSuccess} />
+          <AlertError errors={formErrors} />
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input"
+              value={formData.name}
+              onInput={handleChange}
+            />
+          </div>
+          <div>
+            <label>Age</label>
+            <input
+              type="number"
+              name="age"
+              className="input"
+              value={formData.age}
+              onInput={handleChange}
+            />
+          </div>
+          <div>
+            <label>E-mail</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              value={formData.email}
+              onInput={handleChange}
+            />
+          </div>
+          <input type="submit" className="button" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
