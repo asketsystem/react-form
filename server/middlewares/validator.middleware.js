a67e39afc7d8a9871e250c986967d6d1f4b18b6b
asketsystem <asketsystem1@gmail.com>
2022-03-05 06:31:44 +0000

    Validation middleware

---
+++ server/middlewares/validator.middleware.js
@@ -0,0 +1,33 @@
+const { body } = require("express-validator");
+
+const validate = (method) => {
+  switch (method) {
+    case "handlePerson": {
+      return [
+        body("name")
+          .not()
+          .isEmpty()
+          .withMessage("Name is required")
+          .trim()
+          .escape(),
+        body("age")
+          .not()
+          .isEmpty()
+          .withMessage("Age are required")
+          .isNumeric()
+          .withMessage("Age must be a numeric value")
+          .trim()
+          .escape(),
+        body("email")
+          .not()
+          .isEmpty()
+          .withMessage("E-mail is required")
+          .isEmail()
+          .withMessage("Please insert a valid e-mail")
+          .normalizeEmail(),
+      ];
+    }
+  }
+};
+
+module.exports = { validate };
