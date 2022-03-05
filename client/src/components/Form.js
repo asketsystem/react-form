f3715b84a8974926969f340ec23f2d9806534e10
asketsystem <asketsystem1@gmail.com>
2022-03-05 14:30:34 +0000

    Initialize the form functions

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 export const Form = () => {
   const initialFormData = {
@@ -10,6 +11,51 @@ export const Form = () => {
   const [formData, setFormData] = useState(initialFormData);
   const [formSuccess, setFormSuccess] = useState();
   const [formErrors, setFormErrors] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send POST request
+      await axios.post("https://locahost:5000/api/v1/person", formData);
+
+      // HTTP req successful
+      setFormSuccess("Data received correctly");
+
+      // Reset form data
+      setFormData(initialFormData);
+    } catch (err) {
+      handleErrors(err);
+    }
+  };
+
+  const handleErrors = (err) => {
+    if (err.response.data && err.response.data.errors) {
+      // Handle validation errors
+      const { erros } = err.response.data;
+
+      let errMsg = [];
+      for (let error of erros) {
+        const { msg } = error;
+
+        errorMsg.push(msg);
+      }
+
+      setFormErrors(errorMsg);
+    } else {
+      // Handle generic error
+      setFormErrors(["Oops, there was an error"]);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    setFormErrors([]);
+    setFormSuccess("");
+  };
 
   return <div></div>;
 };
